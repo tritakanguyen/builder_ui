8b3896895763137400e12620065b57e280cc6011
tritakanguyen <ftnguyen@amazon.com>
2025-11-03 23:10:14 -0800

    fix hosting

--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
