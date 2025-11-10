1c1d4ed88d1155a89eae60d7ac037764fbcfcd58
tritakanguyen <ftnguyen@amazon.com>
2025-11-09 22:29:21 -0800

    debug new image tag update

--- src/utils/updateDockerImage.js
+++ src/utils/updateDockerImage.js
@@ -1,6 +1,6 @@
 function generateYamlUpdateCommand(yamlFilePath, serviceName, newImage, hasService) {
   var indent = hasService ? '  ' : '    ';
-  var awkCmd = "awk '/^" + indent + serviceName + ":/{f=1} f && /image:/{sub(/image:.*/, \"image: " + newImage + "\"); f=0} 1' " + yamlFilePath + " > temp && mv temp " + yamlFilePath;
+  var awkCmd = "awk '/^" + indent + serviceName + ":/{f=1} f && /image:/{sub(/image:.*/, \\"image: " + newImage + "\\"); f=0} 1' " + yamlFilePath + " > temp && mv temp " + yamlFilePath;
   var verifyCmd = "sed -n '/^" + indent + serviceName + ":/,/image:/p' " + yamlFilePath;
   return awkCmd + '\n' + verifyCmd;
 }
